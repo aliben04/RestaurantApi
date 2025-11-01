7afa1b34af37b6b701e9008412dfddacea4a9933
Ali Benettoumi <benettoumiali798@example.com>
2025-11-01 02:42:11 +0100

    Mise à jour finale

--- src/main/kotlin/com/restaurant/RestaurantApi/controller/Exeption.kt
+++ src/main/kotlin/com/restaurant/RestaurantApi/controller/Exeption.kt
@@ -7,29 +7,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
-/**
- * هذا الكلاس يراقب كل الـ Controllers في التطبيق
- * ويلتقط الأخطاء المحددة ويعالجها.
- */
+
 @RestControllerAdvice
 class GlobalExceptionHandler {
 
-    /**
-     * هذه الدالة تعالج أخطاء التحقق (@Valid) فقط
-     */
+
     @ExceptionHandler(MethodArgumentNotValidException::class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST) // (إرجاع كود 400 - طلب سيء)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     fun handleValidationExceptions(ex: MethodArgumentNotValidException): Map<String, String> {
 
         val errors = mutableMapOf<String, String>()
 
-        // المرور على كل الأخطاء التي حدثت
+
         ex.bindingResult.fieldErrors.forEach { error ->
             val fieldName = error.field
             val errorMessage = error.defaultMessage ?: "خطأ غير معروف في التحقق"
 
-            // إضافة الخطأ إلى الـ Map
-            // (اسم الحقل -> رسالة الخطأ التي كتبتها)
+
             errors[fieldName] = errorMessage
         }
 
